0bac29a4583cf3551b116dc44883bd15baae192f
Agata <agatatocz@gmail.com>
2019-02-11 18:22:16 +0100

    Updates

---
+++ precache-manifest.fe20f1b6ef40e95177bdb48fa9796e7d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/photo-gallery-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "8f284bb2123808ca2129",
+    "url": "/photo-gallery-app/static/css/main.7323f587.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/photo-gallery-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a346570a27c2117aaa3d",
+    "url": "/photo-gallery-app/static/js/1.a346570a.chunk.js"
+  },
+  {
+    "revision": "dae5ae396726c8283974",
+    "url": "/photo-gallery-app/static/js/runtime~main.dae5ae39.js"
+  },
+  {
+    "revision": "8f284bb2123808ca2129",
+    "url": "/photo-gallery-app/static/js/main.8f284bb2.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/photo-gallery-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/photo-gallery-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/photo-gallery-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a346570a27c2117aaa3d",
+    "url": "/photo-gallery-app/static/css/1.dd7e4a72.chunk.css"
+  },
+  {
+    "revision": "1711d646258fc1447a21e232ea7fbf9c",
+    "url": "/photo-gallery-app/index.html"
+  }
+];
